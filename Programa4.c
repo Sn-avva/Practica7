a898cefc6774abe83952d650f3584dfeb936f857
Samantha Nava <sn7avva@gmail.com>
2025-10-09 14:13:21 -0600

    Create Programa4.c

---
+++ Programa4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main ()
+{
+  int uno, dos, tres;
+printf ("Ingrese 3 números separados por espacios:\n");
+scanf ("%d %d %d", &uno, &dos, &tres);
+if (uno > dos)
+{
+if (dos > tres)
+{
+printf("%d es mayor a %d que es mayor a %d\n", uno, dos, tres);
+}
+else 
+{
+if (uno > tres)
+{
+printf("%d es mayor a %d que es mayor a %d\n", uno, tres, dos);
+}
+else 
+{
+printf("%d es mayor a %d que es mayor a %d\n", tres, uno, dos);
+}
+}
+}
+else 
+{
+if (dos > tres)
+{
+if (tres > uno)
